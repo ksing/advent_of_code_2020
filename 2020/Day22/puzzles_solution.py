d7101c3f847fd2640c1116b829ed1f7b577357fc
Kush Singhal <singhalkushagra@gmail.com>
2021-01-12 22:18:27 +0100

    Complete Day22: had missed an important part of the puzzle - number of cards in the subgame must be equal to the cards drawn *facepalm*

--- 2020/Day22/puzzles_solution.py
+++ 2020/Day22/puzzles_solution.py
@@ -1,6 +1,7 @@
 import re
 from collections import deque
 from copy import deepcopy
+from itertools import islice
 
 from ..helper_functions import get_input_file_name, timer
 
@@ -12,12 +13,12 @@ def main():
             parse_player_decks(player_data) for player_data in f.read().strip().split('\n\n')
         )
     print(f'Puzzle 1 solution: {puzzle1_solution(deepcopy(dict_players))}')
-    #print(f'Puzzle 2 solution: {puzzle2_solution(deepcopy(dict_players))}')
+    print(f'Puzzle 2 solution: {puzzle2_solution(deepcopy(dict_players))}')
 
 
 def parse_player_decks(player_data):
     input_rows = player_data.splitlines()
-    player_number = int(re.match(r'Player (\d+):', input_rows[0]).group(1))
+    player_number = int(re.match(r'Player (\d+):', input_rows[0]).group(1)) - 1
     data = deque(int(row.strip()) for row in input_rows[1:])
     return player_number, data
 
@@ -30,46 +31,52 @@ def puzzle1_solution(dict_players):
 
 
 def _play_combat_game(dict_players):
-    while dict_players[1] and dict_players[2]:
-        card1, card2 = dict_players[1].popleft(), dict_players[2].popleft()
-        if card1 > card2:
-            dict_players[1] += [card1, card2]
-        else:
-            dict_players[2] += [card2, card1]
+    while dict_players[0] and dict_players[1]:
+        cards = dict_players[0].popleft(), dict_players[1].popleft()
+        winner = cards.index(max(cards))
+        dict_players[winner].extend([cards[winner % 2], cards[(winner + 1) % 2]])
     print(dict_players)
-    return dict_players[1] + dict_players[2]
+    return dict_players[0] + dict_players[1]
 
 
 @timer
 def puzzle2_solution(dict_players):
     # https://adventofcode.com/2020/day/22#part2
     # print(dict_players)
-    winner, dict_players = _play_recurse_combat_game(dict_players)
+    winner, dict_players = _play_recursive_combat_game(dict_players)
     print(dict_players)
     print(f"Winner: {winner}")
     return _get_deck_score(dict_players[winner])
 
 
-def _play_recurse_combat_game(dict_players):
-    deck_snapshots = [(dict_players[1].copy(), dict_players[2].copy())]
-    winner = 0
-    while dict_players[1] and dict_players[2]:
-        # print(deck_snapshots)
-        cards = dict_players[1].popleft(), dict_players[2].popleft()
-        if cards[0] <= len(dict_players[1]) and cards[1] <= len(dict_players[2]):
-            print('Starting a sub-game of Recursive combat')
-            print(cards)
-            print(dict_players.values())
-            winner, _ = _play_recurse_combat_game(deepcopy(dict_players))
+def _create_cards_snapshot(player_1_deck, player_2_deck):
+    return (
+        ' '.join([str(card) for card in player_1_deck])
+        + '\t'
+        + ' '.join([str(card) for card in player_2_deck])
+    )
+
+
+def _play_recursive_combat_game(dict_players):
+    deck_snapshots = set()
+    winner = -1
+    while dict_players[0] and dict_players[1]:
+        snapshot = _create_cards_snapshot(dict_players[0], dict_players[1])
+        if snapshot in deck_snapshots:
+            # print('Configuration already exists')
+            return 0, dict_players
         else:
-            winner = cards.index(max(cards)) + 1
-        dict_players[winner] += [cards[(winner - 1) % 2], cards[winner % 2]]
-        if (dict_players[1], dict_players[2]) in deck_snapshots:
-            print('Configuration already exists')
-            return 1, dict_players
-        deck_snapshots.append((dict_players[1].copy(), dict_players[2].copy()))
-    # print(dict_players)
-    # print(f"Game {game_num} Winner: {winner}")
+            deck_snapshots.add(snapshot)
+        cards = dict_players[0].popleft(), dict_players[1].popleft()
+        if cards[0] <= len(dict_players[0]) and cards[1] <= len(dict_players[1]):
+            subgame_dict_players = {
+                i: deque(islice(dict_players[i], 0, cards[i]))
+                for i in (0, 1)
+            }
+            winner, _ = _play_recursive_combat_game(subgame_dict_players)
+        else:
+            winner = cards.index(max(cards))
+        dict_players[winner].extend([cards[winner % 2], cards[(winner + 1) % 2]])
     return winner, dict_players
 
 
