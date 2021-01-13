f821bc802c576aa1a99a7d730846310d52fd6bca
Kushagra Singhal <singhalkushagra@gmail.com>
2021-01-13 20:13:49 +0100

    Standardize Day2 puzzles solution

--- 2020/Day2/puzzles_solution.py
+++ 2020/Day2/puzzles_solution.py
@@ -11,29 +11,33 @@ def main():
     file_name = get_input_file_name(__file__)
     with open(file_name, 'r') as f:
         input_data = f.readlines()
-    print(f'Puzzle 1: {sum(_is_valid_passwd_puzzle1(line.strip()) for line in input_data)}')
-    print(f'Puzzle 2: {sum(_is_valid_passwd_puzzle2(line.strip()) for line in input_data)}')
+    print(f'Puzzle 1: {puzzle1_solution(input_data)}')
+    print(f'Puzzle 2: {puzzle2_solution(input_data)}')
 
 
 @timer
-def _is_valid_passwd_puzzle1(line):
-    try:
-        min_occur, max_occur, policy_char, passwd = REG.match(line).groups()
-        return int(min_occur) <= Counter(passwd)[policy_char] <= int(max_occur)
-    except AttributeError:
-        return False
+def puzzle1_solution(input_data):
+    def _is_valid_passwd(line):
+        try:
+            min_occur, max_occur, policy_char, passwd = REG.match(line).groups()
+            return int(min_occur) <= Counter(passwd)[policy_char] <= int(max_occur)
+        except AttributeError:
+            return False
+    return sum(_is_valid_passwd(line.strip()) for line in input_data)
 
 
 @timer
-def _is_valid_passwd_puzzle2(line):
-    try:
-        min_index, max_index, policy_char, passwd = REG.match(line).groups()
-        return (
-            bool(passwd[int(min_index) - 1] == policy_char) !=
-            bool(len(passwd) >= int(max_index) and passwd[int(max_index) - 1] == policy_char)
-        )  # XOR gate implementation
-    except AttributeError:
-        return False
+def puzzle2_solution(input_data):
+    def _is_valid_passwd(line):
+        try:
+            min_index, max_index, policy_char, passwd = REG.match(line).groups()
+            return (
+                bool(passwd[int(min_index) - 1] == policy_char) !=
+                bool(len(passwd) >= int(max_index) and passwd[int(max_index) - 1] == policy_char)
+            )  # XOR gate implementation
+        except AttributeError:
+            return False
+    return sum(_is_valid_passwd(line.strip()) for line in input_data)
 
 
 main()
